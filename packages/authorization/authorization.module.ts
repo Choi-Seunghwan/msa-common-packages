6efca2430596e27aec31bf31ec3f098e119dcd42
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-03-03 23:46:29 +0900

    feat: update authorization module

--- packages/authorization/authorization.module.ts
+++ packages/authorization/authorization.module.ts
@@ -1,15 +1,21 @@
 import { Module, DynamicModule } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthorizationService } from "./authorization.service";
 
 @Module({})
 export class AuthorizationModule {
-  static register(jwtSecret: string): DynamicModule {
+  static register(): DynamicModule {
     return {
       module: AuthorizationModule,
       imports: [
-        JwtModule.register({
-          secret: jwtSecret,
+        ConfigModule,
+        JwtModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            secret: configService.getOrThrow<string>("JWT_SECRET"),
+          }),
+          inject: [ConfigService],
         }),
       ],
       providers: [AuthorizationService],
